7ac3b0cf32a3b662cb2e0676ccb04d78fee00ad5
Jesse Himmelstein <jesse@heavenstone.net>
2019-08-28 15:48:06 +0200

    Translated to ES Module syntax

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-module.exports = doto
-
-function doto (target, steps, callback) {
+export default function doto(target, steps, callback) {
   if (callback) {
     return doto.async(target, steps, callback);
   } else {
@@ -8,36 +6,37 @@ function doto (target, steps, callback) {
   }
 }
 
-doto.sync = function (target, steps) {
-  for (var i = 0, len = steps.length; i < len; i++) {
+doto.sync = function(target, steps) {
+  for (let i = 0, len = steps.length; i < len; i++) {
     apply(steps[i], target);
   }
   return target;
-}
+};
 
-doto.async = function (target, steps, done) {
-  var i = 0;
+doto.async = function(target, steps, done) {
+  let i = 0;
   next();
 
-  function next (err) {
+  function next(err) {
     if (err) return done(err);
-    var step = steps[i++]
+    const step = steps[i++];
     if (!step) done(null, err);
-    else try {
-      apply(step, target, next);
-    } catch (err) {
-      done(err);
-    }
+    else
+      try {
+        apply(step, target, next);
+      } catch (err) {
+        done(err);
+      }
   }
-}
+};
 
-function apply (step, target, next) {
-  if (typeof step == 'function') {
-    return next ? step(target, next) : step(target)
+function apply(step, target, next) {
+  if (typeof step == "function") {
+    return next ? step(target, next) : step(target);
   } else {
-    var args = step.slice(1)
-    args.unshift(target)
-    if (next) args.push(next)
-    return step[0].apply(null, args)
+    const args = step.slice(1);
+    args.unshift(target);
+    if (next) args.push(next);
+    return step[0].apply(null, args);
   }
 }
